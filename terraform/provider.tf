ba384e90c0a1a45c2dbb104af497e2af889db721
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 21:36:13 -0300

    tst2

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,20 +1,19 @@
-# PROVIDER
 terraform {
 
-  required_version = "~> 1.11.3"
+  required_version = "~> 1.13.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.94.1"
+      version = "~> 6.9.0"
     }
   }
 
-}
+  backend "s3" {
+    bucket       = "dori"
+    key          = "tfstate"
+    region       = "us-east-1"
+    use_lockfile = true
+  }
 
-provider "aws" {
-  region                   = "us-east-1"
-  shared_config_files      = [".aws/config"]
-  shared_credentials_files = [".aws/credentials"]
-  profile                  = "iac"
 }
